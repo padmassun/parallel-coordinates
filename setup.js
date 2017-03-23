6442561be8a2673ef2203a7080ab1df3383c3d01
padmassun <padmassun@gmail.com>
2017-03-23 10:08:02 -0400

    added options to customize plot by building type

--- setup.js
+++ setup.js
@@ -1,11 +1,18 @@
+config_file = JSON.parse(get_file("parallel_plots_config.json"));
+//options = get_options()
 function setup() {
+    setup_options()
     //d3.csv("./data/output.csv",function(csv_data){console.log(csv_data)});
     //console.log(unparsed_data(json));
-    raw_data = JSON.parse(get_file("./data/simulations.json"));
     //console.log(raw_data);
-    data = get_unparsed_data(raw_data)
+
+    //data = get_filtered_data('Select All')
+
     //console.log(data);
-    setup_canvas(data)
+
+    //setup_canvas(data)
+
+    update_plot_with_options()
 }
 
 function get_file(name) {
@@ -16,14 +23,27 @@ function get_file(name) {
 }
 
 function setup_canvas(data) {
-    var colorgen = d3.scale.ordinal()
-        .domain(['FullServiceRestaurant', 'LargeHotel', 'LargeOffice', 'MediumOffice', 'MidriseApartment', 'Outpatient', 'PrimarySchool', 'QuickServiceRestaurant', 'RetailStandalone', 'SecondarySchool', 'SmallHotel', 'SmallOffice', 'RetailStripmall', 'Warehouse'])
+    var color_by_bldg_type = d3.scale.ordinal()
+        .domain(config_file.buildings)
         .range(["#3366cc", "#dc3912", "#ff9900", "#109618",
             "#990099", "#0099c6", "#dd4477", "#66aa00",
             "#b82e2e", "#316395", "#994499", "#22aa99",
-            "#aaaa11", "#6633cc",
-            //"#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"
+            "#aaaa11", "#6633cc", "#e67300", "#8b0707",
+            "#651067", "#329262", "#5574a6", "#3b3eac"
         ]);
+    var red_to_cyan = d3.scale.linear()
+        .domain([3000,8000])
+        .range(["cyan", "red"]);
+
+    var color = function (d) { /*console.log(d);*/
+        option = d3.select("#bldgType").property("value")
+        if (option == "Select All") {
+            return color_by_bldg_type(d["Building Type"]);
+        } else {
+            return red_to_cyan(d["HDD"]);
+        }
+
+    };
 
     var parcoords = d3.parcoords()("#chart")
         .margin({
@@ -37,25 +57,34 @@ function setup_canvas(data) {
         .smoothness(0.2)
         .bundlingStrength(0)
         .data(data)
-        .color(function (d) {/*console.log(d);*/ return colorgen(d["Building Type"]); })
+        .color(function (d) {
+            return color(d)
+        })
         .mode("queue")
         .render()
         .createAxes()
-        .brushMode("1D-axes");
+        .brushMode("1D-axes-multi");
 
-    //console.log(parcoords.brushModes())
+    //console.log(parcoords.dimensions())
 }
 
-function get_unparsed_data(json) {
+function get_filtered_data(option) {
     //may be i could also get another input as a filter, use if statements to select groups (end_uses or end_uses_eui) 
     //based on filter and add or remove blocks from title... the filter could be an input from eventlisteners on the checkboxes
-    //parallel_plots_config.json
-    config_file = JSON.parse(get_file("parallel_plots_config.json"));
+    //parallel_plots_config.json 
     //console.log(config_file);
+    json = JSON.parse(get_file("./data/simulations.json"));
     title = config_file.title;
+    if (option != "Select All") {
+        delete title["Building Type"]
+    }
+    console.log(title);
     data = []
     for (i = 0; i < json.length; i++) {
         x = {};
+        if (option != "Select All" && json[i].building_type != option) {
+            continue;
+        }
         for (var key in title) {
             if (title.hasOwnProperty(key)) {
                 command = "x[\"" + key + "\"] = " + "json[i]." + title[key];
@@ -67,4 +96,29 @@ function get_unparsed_data(json) {
         data.push(x)
     };
     return data
+}
+
+function setup_options() {
+    options = config_file.buildings;
+    options.unshift("Select All")
+    console.log(options)
+    var bldgType = d3.select('#bldgType')
+    bldgType.selectAll('option')
+        .data(options)
+        .enter()
+        .append('option')
+        .text(function (d) {
+            return d;
+        });
+
+}
+
+function update_plot_with_options() {
+    d3.select('#update_bldgType').on('click', function () {
+        option = d3.select("#bldgType").property("value")
+        console.log(option + " < Option");
+        data = get_filtered_data(option)
+        d3.selectAll("g > *").remove()
+        setup_canvas(data)
+    })
 }
