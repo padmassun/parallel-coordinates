1ea76c38a09ce0fa75156437a0100afa7367641f
padmassun <padmassun@gmail.com>
2017-03-30 14:39:14 -0400

    moved all files to src

---
+++ server_old.js
@@ -0,0 +1,59 @@
+var http = require('http'),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+
+//Lets define a port we want to listen to
+const PORT = 8080;
+
+var index = fs.readFileSync('index.html');
+
+//We need a function which handles requests and send response
+function handleRequest(request, response) {
+    /*response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(index);
+    */
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
+
+    var contentTypesByExtension = {
+        '.html': "text/html",
+        '.css': "text/css",
+        '.js': "text/javascript"
+    };
+    fs.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, { "Content-Type": "text/plain" });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, { "Content-Type": "text/plain" });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+            var headers = {};
+            var contentType = contentTypesByExtension[path.extname(filename)];
+            if (contentType) headers["Content-Type"] = contentType;
+            response.writeHead(200, headers);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
